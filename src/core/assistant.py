be18cbfdbb271b6cea169f09bd9c2790fc3693ad
Lucas Pompeii <87397387+Pompei1@users.noreply.github.com>
2026-02-27 13:04:41 -0300

    initial_comands assistant.py

--- src/core/assistant.py
+++ src/core/assistant.py
@@ -1,1 +1,8 @@
+class Assistant:
+    def __init__(self):
+        self.name = "Car Assistant"
 
+    def respond(self, command: str):
+        if "hora" in command:
+            return "Ainda vou aprender a falar as horas."
+        return "Comando não reconhecido."
