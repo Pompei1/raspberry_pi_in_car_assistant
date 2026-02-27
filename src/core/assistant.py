57e0939d54c8def29c0258158e67e5162cb3dd7f
Lucas Pompeii <87397387+Pompei1@users.noreply.github.com>
2026-02-27 14:44:25 -0300

    initial_comands_v3 assistant.py

--- src/core/assistant.py
+++ src/core/assistant.py
@@ -6,3 +6,18 @@ class Assistant:
         if "hora" in command:
             return "Ainda vou aprender a falar as horas."
         return "Comando não reconhecido."
+
+from intents.time_intent import TimeIntent
+from intents.fallback_intent import FallbackIntent
+
+class Assistant:
+    def __init__(self):
+        self.intents = [
+            TimeIntent(),
+            FallbackIntent()
+        ]
+
+    def process(self, command: str) -> str:
+        for intent in self.intents:
+            if intent.can_handle(command):
+                return intent.handle(command)
